deb682402b5eaaff27a9835a53159ff90f279447
Hamlethan27 <hamlethan1999@gmail.com>
2022-11-18 14:25:40 +0600

    news modals almost done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -62,3 +62,34 @@ document.addEventListener('DOMContentLoaded', () => {
         return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
     }
 })
+
+
+
+
+
+
+const btns = document.querySelectorAll('.button');
+const modalOverlay = document.querySelector('.modal-overlay');
+const modals = document.querySelectorAll('.modal');
+
+btns.forEach((el) => {
+    el.addEventListener('click', (e) => {
+        let path = e.currentTarget.getAttribute('data-path');
+        console.log(path);
+        modals.forEach((el) => {
+            el.classList.remove('modal-overlay--visible');
+        });
+        document.querySelector(`[data-target="${path}"]`).classList.add('modal--visible');
+        modalOverlay.classList.add('modal-overlay--visible');
+    });
+});
+
+
+modalOverlay.addEventListener('click', (e) => {
+    if(e.target == modalOverlay) {
+        modalOverlay.classList.remove('modal-overlay--visible');
+        modals.forEach((el) => {
+            el.classList.remove('modal--visible');
+        });
+    }
+});
